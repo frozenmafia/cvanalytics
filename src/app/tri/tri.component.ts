fbcb22710e81221e3a35afeed08dd8e91e8df6a5
frozenmafia <shivajay295@gmail.com>
2022-05-18 02:47:02 +0530

    first2

---
+++ src/app/tri/tri.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tri',
+  templateUrl: './tri.component.html',
+  styleUrls: ['./tri.component.css']
+})
+export class TriComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
